770803eab62402d9cfd396939c9e737c9c2b94f9
2050gui2050 <144683702+2050gui2050@users.noreply.github.com>
2024-10-29 11:25:41 -0300

    ,

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
-}
-cartao.innerHTML = `
+    cartao.innerHTML = `
 <div class="cartao__conteudo">
 <h3>Programação</h3>
 <div class="cartao__conteudo__pergunta">
@@ -14,3 +13,4 @@ cartao.innerHTML = `
 </div>
 </div>
 `container.appendChild(cartao)
+}
